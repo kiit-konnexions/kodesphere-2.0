94e645209247af08ec4ef0248158471aaa4360ba
Supreet0 <supreetpatel2004@gmail.com>
2025-03-21 02:08:15 +0530

    changes

--- app/digital-id/[teamId]/page.jsx
+++ app/digital-id/[teamId]/page.jsx
@@ -4,7 +4,7 @@ const page = ({ firstName, lastName, group }) => {
   return (
     <div className="w-[450px] md:w-[350px] relative pointer-events-none select-none">
       <img src="/idcard.png" className="h-full w-full" alt="idcard" />
-      <div className="absolute bottom-[70px] md:bottom-[85px] left-9 md:left-11">
+      <div className="absolute p-14 bottom-[70px] md:bottom-[85px] left-9 md:left-11">
         <h1 className="text-[20px] font-semibold font-poppins text-[#345969]">
           {`${firstName} ${lastName}`}
         </h1>
