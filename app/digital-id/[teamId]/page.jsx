4d784d70628999eb2316addf635874a46a0563bd
kiit-konnexions <it.society@kiit.ac.in>
2025-03-22 13:53:41 +0530

    fix:removed team name

--- app/digital-id/[teamId]/page.jsx
+++ app/digital-id/[teamId]/page.jsx
@@ -34,13 +34,17 @@ const page = () => {
     <div className={`${spaceGrotesk.className} flex min-h-screen flex-wrap items-center justify-center gap-10 bg-gray-200 p-2`}>
     {participants && participants?.map((participant)=>(<div key={participant.UserId} className="w-[450px] md:w-[350px] relative pointer-events-none select-none p-0">
         <img src="/idcard.png" className="h-full w-full rounded-xl shadow-xl" alt="idcard" />
-        <h1 className="absolute top-[45%] text-2xl font-bold left-[5%] font-poppins text-[#345969]">
-          {participant.name.split(" ")[0]}<br/>
-          {participant.name.split(" ").slice(1,participant.name.split(" ").length).map((p)=>(
-            p+" "
-          ))}
+        <h1 className="absolute top-[45%] font-bold left-[5%] font-poppins text-black flex flex-col items-start justify-start">
+          <span className="text-5xl font-bold">
+            {participant.name.split(" ")[0]}
+          </span>
+          <span className="text-2xl font-normal text-black/60">
+            {participant.name.split(" ").slice(1,participant.name.split(" ").length).map((p)=>(
+              p+" "
+            ))}
+          </span>
         </h1>
-        <p className="font-poppins absolute top-[58%] text-xl text-black font-bold left-[5%]">{participant.Team.TeamName}{" - "}{participant.Team.TeamId}</p>
+        {/* <p className="font-poppins absolute top-[60%] text-sm text-black font-bold left-[5%]">{participant.Team.TeamName}{" - "}{participant.Team.TeamId}</p> */}
       </div>))}
     {participants===null && !loading && <span>
         Team Details Not Found
