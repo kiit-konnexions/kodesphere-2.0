e9dc82f2d57ea85198e09a8b36a4f7425290f4bf
Supreet0 <supreetpatel2004@gmail.com>
2025-03-30 01:28:22 +0530

    Refactored the ui and the design of the pop up model

--- components/PopupModal.jsx
+++ components/PopupModal.jsx
@@ -4,19 +4,27 @@ export default function PopupModal({ isOpen, onClose }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-transparent bg-opacity-100 z-50">
-      <div className="bg-white p-6 rounded-2xl shadow-lg max-w-md w-full relative">
-        <button
-          onClick={onClose}
-          className="absolute top-3 right-3 text-gray-500 hover:text-black"
-        >
-          <X size={24} />
-        </button>
-        <h1 className="text-xl font-bold text-black">Announcements</h1>
-        <p className="text-sm text-gray-600 mt-2">
-          The Event has postponed from 31st March 2025 and the new date is 6th
-          April 2025.
-        </p>
+    <div className="fixed z-10 inset-0 h-full w-full bg-gradient-to-b from-transparent to-black/50 flex items-center justify-center">
+      <div className="w-full max-w-md bg-white rounded-xl relative">
+        <div className="flex items-center justify-between px-5">
+          <img
+            src="/kodespherelogo.png"
+            className="h-12 rounded-xl mt-5 -ml-1"
+          />
+          <button
+            className="h-10 w-10 flex items-center justify-center bg-neutral-100 text-neutral-700 rounded-full"
+            onClick={onClose}
+          >
+            <X size={24} />
+          </button>
+        </div>
+        <div className="px-5 pb-7 mt-2">
+          <h1 className="text-xl font-bold text-black">Announcements</h1>
+          <p className="text-sm leading-7 text-neutral-500">
+            The Hackathon has postponed from 31st March, 2025 and the new date
+            is 6th April, 2025.
+          </p>
+        </div>
       </div>
     </div>
   );
