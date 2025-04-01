8ec5a90449c8e06cccb1258e07cae9b20dbe3656
Supreet0 <supreetpatel2004@gmail.com>
2025-04-01 12:17:20 +0530

    Idcard image changed

--- app/digital-id/[teamId]/page.jsx
+++ app/digital-id/[teamId]/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { getIdCards } from "@/app/actions/getIdCards";
 import { Space_Grotesk } from "next/font/google";
@@ -11,7 +11,7 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 const page = () => {
-  const {teamId} = useParams();
+  const { teamId } = useParams();
 
   const [participants, setParticipants] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -19,50 +19,76 @@ const page = () => {
   const getDetails = async () => {
     setLoading(true);
     const pd = await getIdCards(teamId);
-    if(pd.success){
+    if (pd.success) {
       setParticipants(pd.data);
     }
     setLoading(false);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getDetails();
-  },[])
+  }, []);
 
-  
   return (
-    <div className={`${spaceGrotesk.className} flex min-h-screen items-center justify-center gap-10 bg-gray-200 py-0 md:px-8 px-1 md:flex-row flex-col`}>
-    {participants&&<div className="flex flex-col items-start justify-start p-8">
-      {/* <span className="text-7xl font-bold font-gray-600">Kodessphere 2.0</span> */}
-      <img src="/kodespherelogo.png" alt="kdsphr" className="w-full h-[100px]" />
-      <span className="font-lg font-bold">
-        Your pass to the one and only Kodessphere 2.0 is here 😍
-      </span>
-    </div>}
+    <div
+      className={`${spaceGrotesk.className} flex min-h-screen items-center justify-center gap-10 bg-gray-200 py-0 md:px-8 px-1 md:flex-row flex-col`}
+    >
+      {participants && (
+        <div className="flex flex-col items-start justify-start p-8">
+          {/* <span className="text-7xl font-bold font-gray-600">Kodessphere 2.0</span> */}
+          <img
+            src="/kodespherelogo.png"
+            alt="kdsphr"
+            className="w-full h-[100px]"
+          />
+          <span className="font-lg font-bold">
+            Your pass to the one and only Kodessphere 2.0 is here 😍
+          </span>
+        </div>
+      )}
       <div className="flex h-full w-fit md:flex-row flex-col items-start justify-end gap-5">
-        {participants && participants?.map((participant)=>(<div key={participant.UserId} className="md:translate-y-[-100px] w-[380px] md:w-[280px] relative pointer-events-none select-none p-0">
-            <img src="/strap.png" className="h-full w-full translate-y-4" alt="idcard-strap" />
-            <img src="/idcard.png" className="h-full w-full rounded-xl shadow-xl" alt="idcard" />
-            <h1 className="absolute top-[68%] font-bold left-[5%] font-poppins text-black flex flex-col items-start justify-start">
-              <span className="text-4xl font-bold">
-                {participant.name.split(" ")[0]}
-              </span>
-              <span className="text-xl font-normal text-black/60">
-                {participant.name.split(" ").slice(1,participant.name.split(" ").length).map((p)=>(
-                  p+" "
-                ))}
-              </span>
-            </h1>
-            <p className="font-poppins absolute top-[83%] text-lg text-black font-bold left-[5%]">{participant.Team.TeamName}</p>
-          </div>))}
+        {participants &&
+          participants?.map((participant) => (
+            <div
+              key={participant.UserId}
+              className="md:translate-y-[-100px] w-[380px] md:w-[280px] relative pointer-events-none select-none p-0"
+            >
+              <img
+                src="/strap.png"
+                className="h-full w-full translate-y-4"
+                alt="idcard-strap"
+              />
+              <img
+                src="/idcard.png"
+                className="h-full w-full rounded-xl shadow-xl"
+                alt="idcard"
+              />
+              <h1 className="absolute top-[68%] font-bold left-[5%] font-poppins text-black flex flex-col items-start justify-start">
+                <span className="text-4xl font-bold">
+                  {participant.name.split(" ")[0]}
+                </span>
+                <span className="text-xl font-normal text-black/60">
+                  {participant.name
+                    .split(" ")
+                    .slice(1, participant.name.split(" ").length)
+                    .map((p) => p + " ")}
+                </span>
+              </h1>
+              <p className="font-poppins absolute top-[83%] text-lg text-black font-bold left-[5%]">
+                {participant.Team.TeamName}
+              </p>
+            </div>
+          ))}
       </div>
-    {participants===null && !loading && <span className="self-center">
-        Team Details Not Found
-      </span>}
+      {participants === null && !loading && (
+        <span className="self-center">Team Details Not Found</span>
+      )}
 
-    {loading && <span className="self-center justify-self-center">
-        Loading ID Cards...
-      </span>}
+      {loading && (
+        <span className="self-center justify-self-center">
+          Loading ID Cards...
+        </span>
+      )}
     </div>
   );
 };
